15030c686671b2583cf7ffcc89bd9c89f8388ba4
Sandeep Kumar <sksandeepds37@gmail.com>
2026-03-31 20:20:45 +0530

    Fix the image url in data.js file

--- src/data.js
+++ src/data.js
@@ -3,107 +3,105 @@ export const projectDetails = [
     id: 1,
     project_type: "Education",
     project_link: "https://fredutopia.in",
-    img: "/developerprofile/images/fredutopia.jpeg"
+    img: "/images/fredutopia.jpeg",
   },
   {
     id: 2,
     project_type: "Entertainment",
     project_link: "https://bollymovies.com",
-    img: "/developerprofile/images/bollymovies.jpeg"
+    img: "/images/bollymovies.jpeg",
   },
   {
     id: 3,
     project_type: "E-commerce",
     project_link: "https://arihantelectro.com",
-    img: "/developerprofile/images/arihantelectro.jpeg"
+    img: "/images/arihantelectro.jpeg",
   },
   {
     id: 4,
     project_type: "Entertainment",
     project_link: "https://bollygradaamirkhanfilms.bollygrad.com/",
-    img: "/developerprofile/images/amir.jpeg"
+    img: "/images/amir.jpeg",
   },
   {
     id: 5,
     project_type: "Entertainment",
     project_link: "https://bfti.bollygrad.com/",
-    img: "/developerprofile/images/bfti.jpeg"
+    img: "/images/bfti.jpeg",
   },
   {
     id: 6,
     project_type: "Entertainment",
     project_link: "https://bollygradfilms.bollygrad.com/",
-    img: "/developerprofile/images/fims.jpeg"
+    img: "/images/fims.jpeg",
   },
   {
     id: 7,
     project_type: "Entertainment",
     project_link: "https://doctorlotpot.bollygrad.com/",
-    img: "/developerprofile/images/doctorlotpot.jpeg"
+    img: "/images/doctorlotpot.jpeg",
   },
   {
     id: 8,
     project_type: "Entertainment",
     project_link: "https://bollygradhollywood.bollygrad.com/",
-    img: "/developerprofile/images/hollywood.jpeg",
-
+    img: "/images/hollywood.jpeg",
   },
   {
     id: 9,
     project_type: "Entertainment",
     project_link: "https://bollygradmusic.bollygrad.com/",
-    img: "/developerprofile/images/music.jpeg",
+    img: "/images/music.jpeg",
   },
   {
     id: 10,
     project_type: "Entertainment",
     project_link: "https://bollygradsalmankhanfilms.bollygrad.com/",
-    img: "/developerprofile/images/salman.jpeg",
+    img: "/images/salman.jpeg",
   },
   {
     id: 11,
     project_type: "Entertainment",
     project_link: "https://bollygradianragnar.bollygrad.com/",
-    img: "/developerprofile/images/ragnar.jpeg",
-
+    img: "/images/ragnar.jpeg",
   },
   {
     id: 12,
     project_type: "Entertainment",
     project_link: "https://bollygradtollywood.bollygrad.com/",
-    img: "/developerprofile/images/tollywood.jpeg",
+    img: "/images/tollywood.jpeg",
   },
-]
+];
 
 export const skills = [
   {
     id: 1,
-    skillName: 'Html',
-    skillLogo: "/developerprofile/images/html.jpg"
+    skillName: "Html",
+    skillLogo: "/images/html.jpg",
   },
   {
     id: 2,
-    skillName: 'Css',
-    skillLogo: "/developerprofile/images/css.jpg"
+    skillName: "Css",
+    skillLogo: "/images/css.jpg",
   },
   {
     id: 3,
-    skillName: 'Javascript',
-    skillLogo: "/developerprofile/images/javascript.jpg"
+    skillName: "Javascript",
+    skillLogo: "/images/javascript.jpg",
   },
   {
     id: 4,
-    skillName: 'Php',
-    skillLogo: "/developerprofile/images/php.jpg"
+    skillName: "Php",
+    skillLogo: "/images/php.jpg",
   },
   {
     id: 5,
-    skillName: 'MySql',
-    skillLogo: "/developerprofile/images/mysql.jpg"
+    skillName: "MySql",
+    skillLogo: "/images/mysql.jpg",
   },
   {
     id: 6,
-    skillName: 'React',
-    skillLogo: "/developerprofile/images/react.jpg"
+    skillName: "React",
+    skillLogo: "/images/react.jpg",
   },
-]
+];
